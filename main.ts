941100f14dd2a94bd2b60f324d9f5c12d095e42e
PtiCalin <143633151+PtiCalin@users.noreply.github.com>
2025-06-24 12:25:37 -0400

    Add vault image description features

--- main.ts
+++ main.ts
@@ -1,134 +1,193 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Notice, Plugin, PluginSettingTab, Setting, TFile, WorkspaceLeaf, Menu, ItemView } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface DescriptionMode {
+  name: string;
+  prompt: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface ImageDescriptionSettings {
+  ollamaUrl: string;
+  modes: DescriptionMode[];
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: ImageDescriptionSettings = {
+  ollamaUrl: 'http://localhost:11434/api',
+  modes: [
+    {
+      name: 'Default',
+      prompt: 'Describe the image.'
+    }
+  ]
+};
 
-	async onload() {
-		await this.loadSettings();
+const VIEW_TYPE = 'image-description-panel';
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+export default class VaultImageDescriptionPlugin extends Plugin {
+  settings: ImageDescriptionSettings;
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+  async onload() {
+    await this.loadSettings();
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    this.addRibbonIcon('image-file', 'Describe Image', () => {
+      this.activatePanel();
+    });
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    this.registerView(VIEW_TYPE, (leaf: WorkspaceLeaf) => new ImageDescriptionView(leaf, this));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    this.addCommand({
+      id: 'open-image-description-panel',
+      name: 'Open Image Description Panel',
+      callback: () => this.activatePanel(),
+    });
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    this.addSettingTab(new ImageDescriptionSettingTab(this.app, this));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    this.registerEvent(
+      this.app.workspace.on('file-menu', (menu, file) => {
+        if (file instanceof TFile && /(png|jpg|jpeg|gif|svg)$/i.test(file.extension)) {
+          const subMenu = new Menu();
+          this.settings.modes.forEach(mode => {
+            subMenu.addItem(item =>
+              item.setTitle(mode.name).onClick(() => this.describeFile(file, mode))
+            );
+          });
+          menu.addItem(item => item.setTitle('Describe Image').setSubmenu(subMenu));
+        }
+      })
+    );
+  }
 
-	onunload() {
+  async describeFile(file: TFile, mode: DescriptionMode) {
+    new Notice(`Would describe ${file.name} using mode "${mode.name}"`);
+  }
 
-	}
+  async activatePanel() {
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE);
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+    const leaf = this.app.workspace.getRightLeaf(false);
+    await leaf.setViewState({ type: VIEW_TYPE, active: true });
+    this.app.workspace.revealLeaf(leaf);
+  }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  onunload() {
+    this.app.workspace.detachLeavesOfType(VIEW_TYPE);
+  }
+
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ImageDescriptionView extends ItemView {
+  plugin: VaultImageDescriptionPlugin;
+  dropEl: HTMLElement;
+  resultEl: HTMLElement;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  constructor(leaf: WorkspaceLeaf, plugin: VaultImageDescriptionPlugin) {
+    super(leaf);
+    this.plugin = plugin;
+  }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+  getViewType() {
+    return VIEW_TYPE;
+  }
+
+  getDisplayText() {
+    return 'Vault Image Description';
+  }
+
+  async onOpen() {
+    const container = this.containerEl.children[1];
+    container.empty();
+
+    container.createEl('h2', { text: 'Vault Image Describer' });
+
+    this.dropEl = container.createEl('div', { cls: 'drop-area', text: 'Drop image here' });
+    this.resultEl = container.createEl('pre');
+
+    this.dropEl.ondragover = (e) => { e.preventDefault(); };
+    this.dropEl.ondrop = (e) => {
+      e.preventDefault();
+      const files = e.dataTransfer?.files;
+      if (files && files.length > 0) {
+        this.resultEl.setText(`Would describe ${files[0].name}`);
+      }
+    };
+  }
+
+  async onClose() {
+    // cleanup if needed
+  }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class ImageDescriptionSettingTab extends PluginSettingTab {
+  plugin: VaultImageDescriptionPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: VaultImageDescriptionPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
+    containerEl.empty();
 
-		containerEl.empty();
+    new Setting(containerEl)
+      .setName('Ollama API URL')
+      .setDesc('Endpoint for the local Ollama server.')
+      .addText(text =>
+        text
+          .setPlaceholder('http://localhost:11434/api')
+          .setValue(this.plugin.settings.ollamaUrl)
+          .onChange(async value => {
+            this.plugin.settings.ollamaUrl = value;
+            await this.plugin.saveSettings();
+          })
+      );
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    containerEl.createEl('h3', { text: 'Description Modes' });
+
+    this.plugin.settings.modes.forEach((mode, index) => {
+      const setting = new Setting(containerEl);
+      setting
+        .addText(text =>
+          text
+            .setPlaceholder('Name')
+            .setValue(mode.name)
+            .onChange(async value => {
+              mode.name = value;
+              await this.plugin.saveSettings();
+            })
+        )
+        .addText(text =>
+          text
+            .setPlaceholder('Prompt')
+            .setValue(mode.prompt)
+            .onChange(async value => {
+              mode.prompt = value;
+              await this.plugin.saveSettings();
+            })
+        )
+        .addExtraButton(btn => {
+          btn.setIcon('cross').setTooltip('Delete').onClick(async () => {
+            this.plugin.settings.modes.splice(index, 1);
+            this.display();
+            await this.plugin.saveSettings();
+          });
+        });
+    });
+
+    new Setting(containerEl)
+      .addButton(btn =>
+        btn.setButtonText('Add Mode').onClick(async () => {
+          this.plugin.settings.modes.push({ name: 'New Mode', prompt: '' });
+          this.display();
+          await this.plugin.saveSettings();
+        })
+      );
+  }
 }
